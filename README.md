a352c55ec86301b8cea625656103ebdc0b43d81a
Martin Masevski <martin.masevski@gmail.com>
2021-05-29 13:58:05 +0200

    Implemented

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # react-07-slider
+
+#ToDos:
+- add some visual effect to show the time of the auto slider
--- src/App.js
+++ src/App.js
@@ -1,9 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  const nextSlide = () => setIndex((index + 1) % people.length);
+  const prevSlide = () => setIndex((index - 1 + people.length) % data.length);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      nextSlide();
+    }, 5000);
+    // clear setInterval
+    return () => clearInterval(slider);
+  }, [index]);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
+          return (
+            <article kay={id} className={position}>
+              <img src={image} alt={title} className="person-img" />
+              <h4>{name}</h4>
+              <p className="my-title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+
+        <button className="prev" onClick={prevSlide}>
+          <FiChevronLeft />
+        </button>
+
+        <button className="next" onClick={nextSlide}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
